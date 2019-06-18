73c0c59584dcf66e0d338d5394f4b48a5546e962
Leonardo Santos <leonardossev@gmail.com>
2019-06-18 18:57:46 -0300

    Creates a Book class

---
+++ src/GradeBook/Book.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Gradebook
+{
+    class Book
+    {
+        private List<double> grades;
+        private string name;
+
+        public Book(string name)
+        {
+            this.grades = new List<double>();
+            this.name = name;
+        }
+    }
+}
+
