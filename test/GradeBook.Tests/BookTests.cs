7903b2d4c2b36eb4852c42c6ad0c87d4de0165a1
Leonardo Santos <leonardossev@gmail.com>
2019-06-19 01:36:31 -0300

    Implements a test for calculating the statistics

---
+++ test/GradeBook.Tests/BookTests.cs
@@ -0,0 +1,23 @@
+using System;
+using Xunit;
+
+namespace GradeBook.Tests
+{
+    public class BookTests
+    {
+        [Fact]
+        public void BookCalculatesStatistics()
+        {
+            var book = new Book("");
+            book.AddGrade(10.0);
+            book.AddGrade(5.0);
+            book.AddGrade(7.5);
+
+            var statistics = book.GetStatistics();
+
+            Assert.Equal(10.0, statistics.High);
+            Assert.Equal(5.0, statistics.Low);
+            Assert.Equal(7.5, statistics.High);
+        }
+    }
+}
