2066f4fa0d8050e676fdb82b6e56cb7ce3c86354
Leonardo Santos <leonardossev@gmail.com>
2019-06-19 14:30:29 -0300

    Implements a method for Letter definition based on the average

--- src/GradeBook/Statistics.cs
+++ src/GradeBook/Statistics.cs
@@ -7,5 +7,29 @@ namespace GradeBook
         public double High;
 
         public double Average;
+
+        public char Letter;
+
+        public void DefineAverageLetter()
+        {
+            switch(this.Average)
+            {
+                case var average when average > 9.0:
+                    this.Letter = 'A';
+                    break;
+                case var average when average > 8.0:
+                    this.Letter = 'B';;
+                    break;
+                case var average when average > 7.0:
+                    this.Letter = 'C';
+                    break;
+                case var average when average > 6.0:
+                    this.Letter = 'D';
+                    break;
+                default:
+                    this.Letter = 'E';
+                    break;
+            }
+        }
     }
 }
